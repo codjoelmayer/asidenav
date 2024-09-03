71f711f76f376e40e6212c0428dcaa7bdaf36bbf
codjoelmayer <tmjoelm4@gmail.com>
2024-09-03 10:10:12 +0200

    Aside nav

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <p class="lead">Copyright</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FooterComp'
+}
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <router-link to="/">Home</router-link>
+            <router-link to="/about">About</router-link>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
